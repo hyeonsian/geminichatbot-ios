07dfb8bd046870657f71a3d32cd2baf79730f6a8
hyeonsian <hyeonsian84@example.com>
2026-02-23 16:35:13 +0900

    Initial Commit

---
+++ GeminiChatBot/GeminiChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiChatBotApp.swift
+//  GeminiChatBot
+//
+//  Created by ASH on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct GeminiChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
